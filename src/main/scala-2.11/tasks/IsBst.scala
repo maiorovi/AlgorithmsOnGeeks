b4d0780cb2d3d488a353f84935b3611717fe2f3a
Egor <maiorov.prog@gmail.com>
2016-08-26 21:52:27 +0300

    implemented isBst using in order traversal

--- src/main/scala-2.11/tasks/IsBst.scala
+++ src/main/scala-2.11/tasks/IsBst.scala
@@ -1,5 +1,7 @@
 package tasks
 
+import scala.reflect.ClassTag
+
 object IsBst extends App {
 
   def isBst(tree:Tree[Int]):Boolean = tree match {
@@ -32,7 +34,62 @@ object IsBst extends App {
   println(isBst(ex1)) // false
 }
 
-trait Tree[+A]
+trait Tree[+A] {
+  def toArray[B >: A](implicit m: ClassTag[B]):Array[B] = {
+    val array = new Array[B](this.size)
+    var pointer:Int = 0
+
+    def loop(acc:Array[B], tree:Tree[B]):Array[B] =  tree match {
+      case Node(value, left, right) => {
+        loop(acc, left)
+        acc(pointer) = value
+        pointer = pointer + 1
+        loop(acc, right)
+      }
+      case Leaf(value) => {
+        acc(pointer) = value
+        pointer = pointer + 1
+        acc
+      }
+    }
+
+    loop(array, this)
+  }
+
+  def size:Int = this match {
+    case Node(value, left, right) => 1 + left.size + right.size
+    case Leaf(value) => 1
+  }
+}
 
 case class Node[A](value:A, left:Tree[A], right:Tree[A]) extends Tree[A]
 case class Leaf[A](value:A) extends Tree[A]
+
+object isBstUsingInOrderTraversal extends App {
+  def isBst(tree:Tree[Int]):Boolean = {
+    val array = tree.toArray
+    var i = 0
+    var result = true
+
+    while(i < array.length - 1) {
+      if (array(i) > array(i + 1)) {
+          result = false
+      }
+      i = i + 1
+    }
+
+    result
+  }
+
+
+  val bst = Node(10, Node(6, Leaf(4), Leaf(7)),Node(15, Leaf(12), Leaf(16)) )
+  val notBst = Node(10, Node(6, Leaf(4), Leaf(7)),Node(15, Leaf(12), Leaf(11)) )
+
+  println(bst.size)
+  println(bst.toArray.mkString(" "))
+
+  println(isBst(bst)) // true
+  println(isBst(notBst)) // false
+  val ex1 =  Node(3, Node(2, Leaf(1), Leaf(4)), Leaf(5))
+  println(isBst(ex1)) // false
+}

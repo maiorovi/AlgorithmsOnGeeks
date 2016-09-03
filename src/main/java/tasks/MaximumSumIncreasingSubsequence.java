20c9e714824e2a9a86dae4cf4341d379759a3c9f
Egor <maiorov.prog@gmail.com>
2016-09-03 19:30:36 +0300

    implemented longest increasing subsequence problem implemented finding of all permutations of a string

--- src/main/java/tasks/MaximumSumIncreasingSubsequence.java
+++ src/main/java/tasks/MaximumSumIncreasingSubsequence.java
@@ -3,42 +3,31 @@ package tasks;
 public class MaximumSumIncreasingSubsequence {
 
     public int findMaxSum(int[] seq) {
-        int max = seq[0];
-        int currentMaxValue = seq[0];
-        int prevMaxValue = -1;
+        int tmp[] = new int[seq.length];
+
+        for (int i = 0; i < seq.length; i++) {
+            tmp[i] = seq[i];
+        }
 
         for (int i = 1; i < seq.length; i++) {
-            if (currentMaxValue < seq[i]) {
-                max += seq[i];
-                prevMaxValue = currentMaxValue;
-                currentMaxValue = seq[i];
-
-            } else {
-                int prevSum = max - seq[i - 1];
-                if (shouldThePrevDigitNeedToBeSkipped(max, max - seq[i - 1], i, seq)) {
-                    max = max - seq[i - 1];
-                    currentMaxValue = prevMaxValue;
-                    i--;
-                } else {
-                    break;
+            for(int j = 0; j < i; j++) {
+                if (seq[i] > seq[j] && tmp[i] < tmp[j] + seq[i]) {
+                    tmp[i] = tmp[j] + seq[i];
                 }
+
             }
         }
+
+        int max = 0;
+
+        for(int i = 0; i < seq.length; i++) {
+            max = Math.max(tmp[i], max);
+        }
+
 
         return max;
     }
 
-    private boolean shouldThePrevDigitNeedToBeSkipped(int currentSum, int prevSumWithoutLastElem, int startPoint, int[] seq) {
-        int futureMax = seq[startPoint];
-        int i = startPoint + 1;
-
-        while (i < seq.length && seq[i-1] < seq[i]) {
-            futureMax += seq[i];
-            i++;
-        }
-
-        return currentSum < prevSumWithoutLastElem + futureMax;
-    }
 
     public static void main(String[] args) {
         MaximumSumIncreasingSubsequence m = new MaximumSumIncreasingSubsequence();
@@ -47,5 +36,7 @@ public class MaximumSumIncreasingSubsequence {
         System.out.println(m.findMaxSum(new int[]{3, 4, 5, 10})); // 22
         System.out.println(m.findMaxSum(new int[]{10, 5, 4, 3})); // 10
         System.out.println(m.findMaxSum(new int[]{3, 2, 6, 4, 5, 1})); // 2 + 4 + 5 = 11
+        System.out.println(m.findMaxSum(new int[]{1,101,2,3,4,5,1000})); // 1102
+
     }
 }
---
+++ src/main/java/tasks/PermutationsOfAString.java
@@ -0,0 +1,37 @@
+package tasks;
+
+public class PermutationsOfAString {
+
+    public void findAllPermutationsOfAString(String s) {
+        permute(s, 0);
+    }
+
+    private void permute(String s, int start) {
+        char[] tmp = s.toCharArray();
+
+        if (s.length() - start == 0) {
+            System.out.println(s);
+            return;
+        }
+
+        for (int i = start; i < s.length(); i++) {
+            permute(swap(s, start, i), start+1);
+//            permute(swap(s, start, i+1), i+1);
+        }
+    }
+
+    private String swap(String s, int i, int j) {
+        char[] tmp = s.toCharArray();
+        char temp = tmp[i];
+        tmp[i] = tmp[j];
+        tmp[j] = temp;
+
+        return new String(tmp);
+    }
+
+    public static void main(String[] args) {
+        PermutationsOfAString perms = new PermutationsOfAString();
+
+        perms.findAllPermutationsOfAString("abcd");
+    }
+}

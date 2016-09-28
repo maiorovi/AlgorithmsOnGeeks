19d88bc33c1ee72dc97eeaf6a5fc41b6309c5052
egor <maiorov.prog@gmail.com>
2016-09-29 00:19:00 +0300

    implemented selection sort

--- src/main/groovy/tasks/findMinSum.groovy
+++ src/main/groovy/tasks/findMinSum.groovy
@@ -4,7 +4,8 @@ int minimum(int a, int b, int c) {
     Math.min(Math.min(a,b),c)
 }
 
-int findMin(int[] arr, int n) {
+int findMin(Integer[] arr)  {
+    int n = arr.length
     int[] sum = new int[n]
 
     sum[0] = arr[0]
@@ -15,5 +16,9 @@ int findMin(int[] arr, int n) {
         sum[i] = arr[i] + minimum(sum[i-1], sum[i-2], sum[i-3])
     }
 
-    return minimum(sum[n-1], sum[n-2], sum[n-3])
+    return minimum(sum[-1], sum[n-2], sum[n-3])
 }
+Integer[] arr = [1,2,3].toArray()
+Integer[] arr1 = [1, 2, 3, 6, 7, 1].toArray()
+println findMin(arr,)
+println findMin(arr1)

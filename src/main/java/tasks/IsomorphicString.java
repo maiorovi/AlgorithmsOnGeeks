95aa23d675e0ce89cfa4aa1459cbaffc67370d06
egor <maiorov.prog@gmail.com>
2016-11-19 07:02:05 +0200

    implemented isomorphic string

---
+++ src/main/java/tasks/IsomorphicString.java
@@ -0,0 +1,29 @@
+package tasks;
+
+import java.util.HashMap;
+
+public class IsomorphicString {
+
+    public boolean isIsomorphic(String source, String target) {
+        HashMap<Character, Character> mapping = new HashMap<>();
+
+        if (source.length() != target.length()) {
+            return false;
+        }
+
+        for (int i = 0; i < source.length(); i++) {
+            if (mapping.containsKey(source.charAt(i))) {
+                if (mapping.get(source.charAt(i)) != target.charAt(i)) {
+                    return false;
+                }
+            } else {
+                mapping.put(source.charAt(i), target.charAt(i));
+            }
+        }
+
+
+        return true;
+
+    }
+
+}
